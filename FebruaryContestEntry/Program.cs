9d8b782c02c4186ea91f57a67160dd0e1d975201
FredericGaspard <46284787+FredericGaspard@users.noreply.github.com>
2020-02-18 00:03:02 +0100

    Update Contest

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -1,20 +1,57 @@
-﻿using System;
+using System;
 
 namespace FebruaryContestEntry
 {
-	class Program
-	{
-		const char block = '▓';
+    class Shape
+    {
+        public int Longeur { get; set; }
+        public int Largeur { get; set; }
 
-		static void Main(string[] args)
-		{
-			// The solution implementation goes here
+        public ConsoleColor Color { get; set; }
 
-			Console.ResetColor();
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
-			Console.ReadLine();
-		}
-	}
+        public ConsoleColor BordCorlor { get; set; }
+
+        public Shape(int longueur, int largeur, ConsoleColor color = ConsoleColor.DarkYellow, ConsoleColor bordColor = ConsoleColor.DarkBlue)
+        {
+            this.Longeur = longueur;
+            this.Largeur = largeur;
+            this.Color = color;
+            this.BordCorlor = bordColor;
+        }
+        private void DrawBlock(char representation, ConsoleColor c)
+        {
+            Console.BackgroundColor = c;
+            Console.Write(representation);
+            Console.ResetColor();
+        }
+        public void DisplayShape(char representation)
+        {
+            var nbXBlock = this.Largeur;
+            var nbYBlock = this.Longeur;
+            var blockColor = this.Color;
+            for (int i = 0; i < nbXBlock; i++)
+            {
+                for (int j = 0; j < nbYBlock; j++)
+                {
+                    if (i == 0 || j == 0 || i == nbXBlock - 1 || j == nbYBlock - 1)
+                        blockColor = this.BordCorlor;
+
+                    DrawBlock(representation, blockColor);
+                    blockColor = this.Color;
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        const char block = '▓';
+        static void Main(string[] args)
+        {
+            var sp = new Shape(30, 15, ConsoleColor.Yellow, ConsoleColor.Green);
+            sp.DisplayShape(block);
+            Console.ReadLine();
+        }
+    }
 }
